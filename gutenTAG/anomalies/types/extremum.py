37952114978b75561beb9c408990aa0c4bb7ffad
wenig <info@pwenig.de>
2021-06-21 17:28:31 +0200

    feat: extremum anomaly for sinus

--- gutenTAG/anomalies/types/extremum.py
+++ gutenTAG/anomalies/types/extremum.py
@@ -24,7 +24,6 @@ class AnomalyExtremum(BaseAnomaly):
     def __init__(self, parameters: AnomalyExtremumParameters):
         self.min = parameters.min
         self.local = parameters.local
-        self.context_window = parameters.context_window
         self.logger = GutenTagLogger()
 
     def generate(self, anomaly_protocol: AnomalyProtocol) -> AnomalyProtocol:
@@ -34,16 +33,22 @@ class AnomalyExtremum(BaseAnomaly):
             base: np.ndarray = sinus.generate_only_base()
             if self.local:
                 base = base[anomaly_protocol.start:anomaly_protocol.end]
-                context = base[:self.context_window]
-                diff = context.max() - context.min()
+                diff = base.max() - base.min()
                 extremum = np.random.rand() * diff
-                min_pos = base.argmin(axis=0)
-                base[min_pos] += extremum
+                if self.min:
+                    pos = base.argmax(axis=0)
+                    base[pos] -= extremum
+                else:
+                    pos = base.argmin(axis=0)
+                    base[pos] += extremum
             else:
                 diff = base.max() - base.min()
                 extremum = (np.random.rand() + 0.5) * diff
                 base = base[anomaly_protocol.start:anomaly_protocol.end]
-                base[length // 2] += extremum
+                if self.min:
+                    base[length // 2] -= extremum
+                else:
+                    base[length // 2] += extremum
             anomaly_protocol.subsequence = base[:, 0]
         else:
             self.logger.warn_false_combination(self.__class__.__name__, anomaly_protocol.base_oscillation_kind.name)
