edcdbed8c1c33f7fffd17bc84d9b01a734843195
wenig <info@pwenig.de>
2021-06-24 13:59:20 +0200

    feat: install with all packages and main inside

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 setup(
     name='gutenTAG',
     version='0.1.0',
-    packages=['gutenTAG'],
+    packages=find_packages(exclude=("tests",)),
     url='https://gitlab.hpi.de/akita/guten-tag',
     license='MIT',
     author='Phillip Wenig',
