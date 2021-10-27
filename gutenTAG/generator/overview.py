5a16e76c8e0708ba6e816bba21865e4d31cc4803
wenig <info@pwenig.de>
2021-10-27 16:11:17 +0200

    feat: extend overview yaml meta data

--- gutenTAG/generator/overview.py
+++ gutenTAG/generator/overview.py
@@ -1,13 +1,24 @@
 import os
-from typing import List, Dict
+from typing import List, Dict, Optional
 import yaml
+import git
 
 
 class Overview:
     FILENAME = "overview.yaml"
+    GUTENTAG_LINK = "https://gitlab.hpi.de/akita/guten-tag"
 
     def __init__(self):
         self.datasets: List[Dict] = []
+        self.seed: Optional[int] = None
+
+        try:
+            self.git_commit_sha = git.Repo(search_parent_directories=True).head.object.hexsha
+        except git.InvalidGitRepositoryError:
+            self.git_commit_sha = None
+
+    def add_seed(self, seed: int):
+        self.seed = seed
 
     def add_dataset(self, config: Dict):
         self.datasets.append(config)
@@ -18,5 +29,11 @@ class Overview:
         for i, dataset in enumerate(self.datasets):
             dataset["generation-id"] = dataset.get("base-oscillation", {}).get("title", i)
             overview["generated-timeseries"].append(dataset)
+
+        overview["meta"] = {}
+        overview["meta"]["seed"] = self.seed
+        overview["meta"]["git_commit_sha"] = self.git_commit_sha
+        overview["meta"]["download_link"] = self.GUTENTAG_LINK
+
         with open(os.path.join(path, self.FILENAME), "w") as f:
             yaml.dump(overview, f)
