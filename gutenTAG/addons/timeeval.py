fix: incorrect period-size calculation in Addon

gutenTAG/addons/timeeval.py
@@ -125,9 +125,12 @@ class TimeEvalAddOn(BaseAddOn):
             frequency = dim.get(PARAMETERS.FREQUENCY)
             kind = dim.get(PARAMETERS.KIND)
             if frequency is None or kind not in [BASE_OSCILLATION_NAMES.SINE, BASE_OSCILLATION_NAMES.ECG, BASE_OSCILLATION_NAMES.RANDOM_MODE_JUMP]:
-                return np.NAN
-            periods.append(int(length / frequency))
-        return np.median(periods).item()
+                periods.append(np.NAN)
+            elif kind in [BASE_OSCILLATION_NAMES.SINE, BASE_OSCILLATION_NAMES.ECG]:
+                periods.append(int(100 / frequency))
+            elif kind == BASE_OSCILLATION_NAMES.RANDOM_MODE_JUMP:
+                periods.append(int(length / frequency))
+        return np.nanmedian(periods).item()
 
     def __init__(self):
         self.df = pd.DataFrame(columns=columns)

tests/test_addons.py
@@ -14,7 +14,7 @@ class TestAddons(unittest.TestCase):
         np.random.seed(seed)
         random.seed(seed)
 
-    def test_timeeval_addon(self):
+    def test_timeeval_addon_rmj(self):
         with open("tests/configs/example-config-rmj.yaml", "r") as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
         args = parse_args(["--config-yaml", "", "--no-save"])
@@ -38,3 +38,28 @@ class TestAddons(unittest.TestCase):
         self.assertAlmostEqual(df["stddev"][0], 0, 1)
         self.assertTrue(np.isnan(df["trend"][0]))
         self.assertEqual(df["period_size"][0], 20)
+
+    def test_timeeval_addon_sine(self):
+        with open("tests/configs/example-config-ecg.yaml", "r") as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+        args = parse_args(["--config-yaml", "", "--no-save"])
+        gutentag = GutenTAG.from_dict(config)
+        gutentag.generate()
+        addon = TimeEvalAddOn()
+        addon.process(overview=gutentag.overview, gutenTAG=gutentag, args=args)
+        df = addon.df
+        self.assertEqual(df["dataset_name"][0], "ecg.unsupervised")
+        self.assertEqual(df["test_path"][0], "ecg/test.csv")
+        self.assertEqual(df["input_type"][0], "univariate")
+        self.assertEqual(df["length"][0], 1000)
+        self.assertEqual(df["dimensions"][0], 1)
+        self.assertEqual(df["contamination"][0], 0.04)
+        self.assertEqual(df["num_anomalies"][0], 1)
+        self.assertEqual(df["min_anomaly_length"][0], 40)
+        self.assertEqual(df["median_anomaly_length"][0], 40)
+        self.assertEqual(df["max_anomaly_length"][0], 40, 2)
+        self.assertEqual(df["train_type"][0], "unsupervised")
+        self.assertAlmostEqual(df["mean"][0], 0.5, 2)
+        self.assertAlmostEqual(df["stddev"][0], 0, 1)
+        self.assertTrue(np.isnan(df["trend"][0]))
+        self.assertEqual(df["period_size"][0], 10)